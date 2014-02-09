b3feadb304668e53f9a023ee9706725c65d381bc
Kiran Panesar <KiranSinghPanesar@googlemail.com>
2014-02-09 16:43:03 +0100

    Added UIKit Dynamics to bounce the media image
    also added comments

--- MXLMediaView/MXLMediaView.m
+++ MXLMediaView/MXLMediaView.m
@@ -14,6 +14,7 @@
 @interface MXLMediaView ()
 
 @property (strong, nonatomic, readwrite) UIImageView *backgroundImageView;
+@property (strong, nonatomic) UIDynamicAnimator *dynamicAnimator;
 
 -(void)showMediaImageView;
 -(void)dismiss:(id)sender;
@@ -31,58 +32,88 @@
     return self;
 }
 
+// Main method to show the media view
 -(void)showImage:(UIImage *)image inParentView:(UIView *)parentView {
+    // Store the parent view and image
     _parentView = parentView;
     _mediaImage = image;
-    
+
+    // Set up self
     [self setFrame:CGRectMake(0.0f, 0.0f, parentView.frame.size.width, parentView.frame.size.height)];
     [self setUserInteractionEnabled:YES];
-    [self addGestureRecognizer:[[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(dismiss:)]];
     
+    // Set up background imageview
+    // This is used to replace the parentView in the backgroud, allowing us to efficiently blur
     _backgroundImageView = [[UIImageView alloc] initWithFrame:parentView.frame];
-    [[_backgroundImageView layer] setMasksToBounds:NO];
-    [_backgroundImageView setImage:[self blurredImageFromView:parentView]];
-    [_backgroundImageView setAlpha:0.0f];
-    
+    [_backgroundImageView setImage:[self blurredImageFromView:parentView]]; // Blur
+    [_backgroundImageView setAlpha:0.0f];                                   // Make invisible
     [self addSubview:_backgroundImageView];
+    
+    // Add self to view stack
     [parentView addSubview:self];
+    
+    // Start showing the actualy image file provided
+    [self showMediaImageView];
 
+    // Animate the background image view opacity
+    // This gives the illusion that the background is being blurred
     [UIView animateWithDuration:0.2 animations:^{
         [_backgroundImageView setAlpha:1.0f];
     } completion:^(BOOL finished) {
         
+        // Once that's complete, hide all the parent view's subviews, except for self
         for (UIView *v in parentView.subviews) {
             if (v != self) {
                 [v setHidden:YES];
             }
         }
         
-        [self showMediaImageView];
         
+        // Animate the scaling of the background image
+        // Giving the illusion that the background view is shrinking a bit
         [UIView animateWithDuration:0.2 animations:^{
+            // CATransform stuff
             CGAffineTransform transform = _backgroundImageView.transform;
             [_backgroundImageView setTransform:CGAffineTransformScale(transform, 0.9, 0.9)];
             [_backgroundImageView setCenter:CGPointMake([UIScreen mainScreen].bounds.size.width/2.0f, [UIScreen mainScreen].bounds.size.height/2.0f)];
             [[UIApplication sharedApplication] setStatusBarHidden:YES withAnimation:UIStatusBarAnimationFade];
+        } completion:^(BOOL finished) {
+            // Once done, add a tap gesture to dismiss self
+            [self addGestureRecognizer:[[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(dismiss:)]];
         }];
     }];
-}
+ }
 
+// Method to show the actual image provided
 -(void)showMediaImageView {
+    // Initialise the imageview
     _mediaImageView = [[UIImageView alloc] initWithImage:_mediaImage];
-    [_mediaImageView setFrame:CGRectZero];
-    [_mediaImageView setCenter:CGPointMake(self.frame.size.width/2.0f, self.frame.size.height/2.0f)];
-    [_mediaImageView setContentMode:UIViewContentModeScaleAspectFit];
+    [_mediaImageView setFrame:CGRectMake(0.0f, -_mediaImageView.frame.size.height, self.frame.size.width, self.frame.size.height)]; // Set it to be off frame
+    [_mediaImageView setContentMode:UIViewContentModeScaleAspectFit];                                                               // Set the content mode
     
-    [self addSubview:_mediaImageView];
+    [self addSubview:_mediaImageView]; // Add to view stack
     
-    [UIView animateWithDuration:0.2 animations:^{
-        [_mediaImageView setFrame:CGRectMake(0.0f, 0.0f,
-                                             self.frame.size.width, self.frame.size.height)];
-    }];
+    // Set up UIKit Dynamic animator instance
+    _dynamicAnimator = [[UIDynamicAnimator alloc] initWithReferenceView:self];
+
+    // Create collision point at the bottom boundary of self
+    UICollisionBehavior *collisionBehaviour = [[UICollisionBehavior alloc] initWithItems:@[_mediaImageView]];
+    [collisionBehaviour addBoundaryWithIdentifier:@"barrier"
+                                fromPoint:CGPointMake(0.0f, self.frame.size.height)
+                                  toPoint:CGPointMake(self.frame.size.width, self.frame.size.height)];
+    
+    // Add gravity effect with 2.5 vertical velocity
+    UIGravityBehavior *gravityBehaviour   = [[UIGravityBehavior alloc] initWithItems:@[_mediaImageView]];
+    [gravityBehaviour setGravityDirection:CGVectorMake(0.0f, 2.5f)];
+    
+    // Add the collision and gravity behaviours to the main animator
+    [_dynamicAnimator addBehavior:collisionBehaviour];
+    [_dynamicAnimator addBehavior:gravityBehaviour];
 }
 
+// Used to dismiss the actual image provided
 -(void)hideMediaImageView {
+    // Animation to shrink it to nothing
     [UIView animateWithDuration:0.2 animations:^{
         CGAffineTransform transform = _mediaImageView.transform;
         [_mediaImageView setTransform:CGAffineTransformScale(transform, 0, 0)];
@@ -90,20 +121,28 @@
     }];
 }
 
+// Method to dismiss self
 -(void)dismiss:(id)sender {
+    // Dismiss actual image provided
     [self hideMediaImageView];
     
+    // Scale background back to fullscreen
     [UIView animateWithDuration:0.2 animations:^{
+        // Scale background image
         CGAffineTransform transform = _backgroundImageView.transform;
         [_backgroundImageView setTransform:CGAffineTransformScale(transform, 1/0.9, 1/0.9)];
         [_backgroundImageView setCenter:CGPointMake([UIScreen mainScreen].bounds.size.width/2.0f, [UIScreen mainScreen].bounds.size.height/2.0f)];
+        
+        // Show status bar
         [[UIApplication sharedApplication] setStatusBarHidden:NO withAnimation:UIStatusBarAnimationNone];
     } completion:^(BOOL finished) {
-
+        // Show all parentview subviews again
         for (UIView *v in _parentView.subviews) {
             [v setHidden:NO];
         }
         
+        // Animate background image opacity to 0
+        // Giving the illusion that the background image is un-blurring
         [UIView animateWithDuration:0.2 animations:^{
             [_backgroundImageView setAlpha:0.0f];
         } completion:^(BOOL finished) {
@@ -112,16 +151,18 @@
     }];
 }
 
+
+#pragma mark Blur methods
+
 -(UIImage *)blurredImageFromView:(UIView *)backgroundView {
     UIImage *backgroundImage = [self captureView:backgroundView];
-    backgroundImage = [backgroundImage applyBlurWithRadius:10.0 tintColor:[UIColor colorWithWhite:0.0f alpha:0.6] saturationDeltaFactor:1.0f maskImage:nil];
+    backgroundImage = [backgroundImage applyBlurWithRadius:6.0 tintColor:[UIColor colorWithWhite:0.0f alpha:0.6] saturationDeltaFactor:1.0f maskImage:nil];
     
     return backgroundImage;
 }
 
 - (UIImage *)captureView:(UIView *)view {
     CGRect screenRect = [[UIScreen mainScreen] bounds];
-    
     UIGraphicsBeginImageContext(screenRect.size);
     
     CGContextRef ctx = UIGraphicsGetCurrentContext();
